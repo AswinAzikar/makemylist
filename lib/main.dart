d86e3d72e5e411b6eb5b080d70c8fc8fd5cf2099
Aswinazikar <azwinaswin8@gmail.com>
2025-01-17 20:10:57 +0530

    Add TODO comment for theme update in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         initialRoute: AppRoutes.home,
         onGenerateRoute: AppRoutes.generateRoute,
         title: 'Flutter Demo',
+        //TODO: theme update
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
