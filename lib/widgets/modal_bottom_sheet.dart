1e1cc39b09f043f41d8e82177a03296a55a43573
Aswinazikar <azwinaswin8@gmail.com>
2025-01-18 10:37:16 +0530

    Refactor ModalBottomSheet to use FractionallySizedBox for responsive height and improve layout structure

--- lib/widgets/modal_bottom_sheet.dart
+++ lib/widgets/modal_bottom_sheet.dart
@@ -2,80 +2,81 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:makemylist/bloc/todo_bloc.dart';
 import 'package:makemylist/models/todo_model.dart';
+import 'package:makemylist/utils/size_utils.dart';
 import 'package:makemylist/widgets/controllers/controllers.dart';
 
 class ModalBottomSheet {
   static void show(BuildContext context) {
-  
     showModalBottomSheet(
       context: context,
-      isScrollControlled:
-          true, // Ensures the keyboard doesn't overlap the modal
+      isScrollControlled: true,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.vertical(top: Radius.circular(16.0)),
       ),
       builder: (_) {
-        return Padding(
-          padding: EdgeInsets.only(
-            left: 16.0,
-            right: 16.0,
-            bottom: MediaQuery.of(context).viewInsets.bottom +
-                16.0, // Adjust for keyboard
-            top: 16.0,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: titleController,
-                decoration: InputDecoration(
-                  labelText: 'Title',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 16.0),
-              TextField(
-                controller: descriptionController,
-                decoration: InputDecoration(
-                  labelText: 'Description',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 24.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                    onPressed: () => Navigator.pop(context),
-                    child: Text('Cancel'),
+        return FractionallySizedBox(
+          heightFactor: 0.8.h,
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: 16.0,
+              right: 16.0,
+              bottom: MediaQuery.of(context).viewInsets.bottom + 16.0,
+              top: 16.0,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: titleController,
+                  decoration: InputDecoration(
+                    labelText: 'Title',
+                    border: OutlineInputBorder(),
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      final title = titleController.text;
-                      final description = descriptionController.text;
-                      if (title.isNotEmpty && description.isNotEmpty) {
-                        context.read<TodoBloc>().add(
-                              AddTodoEvent(
-                                TodoModel(
-                                  title: title,
-                                  description: description,
+                ),
+                SizedBox(height: 16.0),
+                TextField(
+                  controller: descriptionController,
+                  decoration: InputDecoration(
+                    labelText: 'Description',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 24.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: Text('Cancel'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        final title = titleController.text;
+                        final description = descriptionController.text;
+                        if (title.isNotEmpty && description.isNotEmpty) {
+                          context.read<TodoBloc>().add(
+                                AddTodoEvent(
+                                  TodoModel(
+                                    title: title,
+                                    description: description,
+                                  ),
                                 ),
-                              ),
-                            );
-                        Navigator.pop(context);
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text('Please fill in all fields!'),
-                          ),
-                        );
-                      }
-                    },
-                    child: Text('Save'),
-                  ),
-                ],
-              ),
-            ],
+                              );
+                          Navigator.pop(context);
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Please fill in all fields!'),
+                            ),
+                          );
+                        }
+                      },
+                      child: Text('Save'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         );
       },
